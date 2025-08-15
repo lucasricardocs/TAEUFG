bed64c1be7ccdcb380462956aba9f9142a0cbb55
lucasricardocs <lucasricardo@gmail.com>
2025-08-15 08:37:15 -0300

    Update concurso.py

--- concurso.py
+++ concurso.py
@@ -597,7 +597,7 @@ def main():
     display_conteudos_com_checkboxes(df)
     
     titulo_com_destaque("📝 Análise Estratégica da Prova", cor_lateral="#e67e22")
-    colA, colB = st.columns([1, 3])
+    colA, colB = st.columns([2, 3])
     with colA:
         st.altair_chart(bar_questoes_padronizado(ED_DATA), use_container_width=True)
     with colB:
